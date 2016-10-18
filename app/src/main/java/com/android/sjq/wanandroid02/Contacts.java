2d4cddf9eb1a2b758531d4b65047d639c7a41335
lantiankongmo1026@gmail.com <lantiankongmo1026@gmail.com>
2016-10-18 18:17:57 +0800

    加载分类数据

---
+++ app/src/main/java/com/android/sjq/wanandroid02/Contacts.java
@@ -0,0 +1,16 @@
+package com.android.sjq.wanandroid02;
+
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+
+public class Contacts {
+    public static final String BASE_URL = "http://www.xueandroid.com/";
+    public static final String KNOW_SYSTEM_BASE_URL = "http://www.xueandroid.com/hierarchy?courseId=13";
+    public static final String HOME = "首页";
+    public static final String KNOWLEDGE_SYSTEM = "知识体系";
+    public static final String POPULAR_TOPICS = "热门专题";
+    public static final String COMMON_FRAMEWORK_FOR_PROJECTS = "项目常用框架";
+    public static final String HIGH_TECH = "5.x高新技术";
+    public static final String OPEN_SOURCE_PROJECT_CATEGORY = "开源项目分类";
+}
--- app/src/main/java/com/android/sjq/wanandroid02/presenters/MainPresenter.java
+++ app/src/main/java/com/android/sjq/wanandroid02/presenters/MainPresenter.java
@@ -1,14 +1,69 @@
 package com.android.sjq.wanandroid02.presenters;
 
+import android.os.AsyncTask;
+
+import com.android.sjq.wanandroid02.Contacts;
 import com.android.sjq.wanandroid02.base.BasePresenter;
+import com.android.sjq.wanandroid02.modles.ClassifyEntity;
+import com.android.sjq.wanandroid02.tool.Log;
 import com.android.sjq.wanandroid02.views.MainView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/10/18.
  */
 
 public class MainPresenter extends BasePresenter<MainView> {
+    //获取大分类数据
     public void getClassify() {
-        view.setClassify();
+        new AsyncTask<String, Integer, ArrayList<ClassifyEntity>>() {
+            ArrayList<ClassifyEntity> mClassifies = null;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected ArrayList<ClassifyEntity> doInBackground(String... params) {
+                Document document;
+                mClassifies = new ArrayList<>();
+                try {
+                    document = Jsoup.connect(Contacts.BASE_URL).get();
+                    Elements classifiesEle = document.getElementsByClass("menu").select("li");
+                    ClassifyEntity classifyEntity;
+                    for (Element element : classifiesEle) {
+                        classifyEntity = new ClassifyEntity();
+                        classifyEntity.setClassifyName(element.select("li").text());
+                        classifyEntity.setHttp_url(Contacts.BASE_URL + element.select("li").select("a").attr("href"));
+                        mClassifies.add(classifyEntity);
+                    }
+                    Log.print("TAG", mClassifies.toString());
+                } catch (IOException e) {
+                    document = null;
+                    e.printStackTrace();
+                }
+                if (mClassifies != null && mClassifies.size() > 0) {
+                    return mClassifies;
+                } else {
+                    return null;
+                }
+
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<ClassifyEntity> list) {
+                super.onPostExecute(list);
+                //设置界面显示
+                view.setClassify(list);
+            }
+        }.execute();
     }
 }
