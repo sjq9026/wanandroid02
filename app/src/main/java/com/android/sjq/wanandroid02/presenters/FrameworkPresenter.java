0875b6954a0cbb97672901aaf8e80872e607b9c9
lantiankongmo1026@gmail.com <lantiankongmo1026@gmail.com>
2016-10-19 16:08:58 +0800

    常用开源框架

---
+++ app/src/main/java/com/android/sjq/wanandroid02/presenters/FrameworkPresenter.java
@@ -0,0 +1,155 @@
+package com.android.sjq.wanandroid02.presenters;
+
+import android.os.AsyncTask;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.sjq.wanandroid02.Contacts;
+import com.android.sjq.wanandroid02.TApplication;
+import com.android.sjq.wanandroid02.adapters.ClassifyAdapter;
+import com.android.sjq.wanandroid02.adapters.FirstCategoryAdapter;
+import com.android.sjq.wanandroid02.adapters.OnBlogItemClickListener;
+import com.android.sjq.wanandroid02.adapters.OnClassifyItemClickListener;
+import com.android.sjq.wanandroid02.adapters.RecentlyBlogAdapter;
+import com.android.sjq.wanandroid02.base.BasePresenter;
+import com.android.sjq.wanandroid02.modles.ClassifyEntity;
+import com.android.sjq.wanandroid02.modles.RecentlyBlogInfoEntity;
+import com.android.sjq.wanandroid02.views.view.FrameworkView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/10/19.
+ */
+
+public class FrameworkPresenter extends BasePresenter<FrameworkView> implements OnClassifyItemClickListener, OnBlogItemClickListener {
+    private ArrayList<ClassifyEntity> mClassifies;
+    private ArrayList<RecentlyBlogInfoEntity> mRecentlies;
+
+    public void initCategoryData() {
+        new AsyncTask<String, Integer, String>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String result = "";
+                try {
+                    Document doc = Jsoup.connect(Contacts.FRAGMENT_WORK_URL).get();
+                    Elements es = doc.getElementsByClass("nav").get(0).getElementsByTag("a");
+                    ClassifyEntity entity = null;
+                    mClassifies = new ArrayList<ClassifyEntity>();
+                    for (int i = 0; i < es.size(); i++) {
+                        Element e = es.get(i);
+                        entity = new ClassifyEntity();
+                        entity.setClassifyName(e.select("a").text());
+                        entity.setHttp_url(Contacts.BASE_URL + e.select("a").attr("href"));
+                        mClassifies.add(entity);
+                        System.out.println(e.toString() + "\n\n");
+                    }
+                    result = "OK";
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    result = "ERR";
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                if (s.equals("OK")) {
+                    FirstCategoryAdapter adapter = new FirstCategoryAdapter(TApplication.mContext, mClassifies);
+                    adapter.setOnItemClickListener(FrameworkPresenter.this);
+                    view.setCategoryAdapter(adapter);
+                    getBlogData(mClassifies.get(0));
+                } else {
+                    view.showShorToast("数据请求失败！！！");
+                    return;
+                }
+            }
+        }.execute();
+    }
+
+
+    public void getBlogData(final ClassifyEntity entity) {
+        new AsyncTask<String, Integer, String>() {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String result = "";
+                mRecentlies = new ArrayList<RecentlyBlogInfoEntity>();
+                try {
+                    Document doc = Jsoup.connect(entity.getHttp_url()).get();
+                    Elements es = doc.getElementsByClass("article_div");
+                    RecentlyBlogInfoEntity entity = null;
+                    for (int i = 0; i < es.size(); i++) {
+                        Element e = es.get(i);
+                        entity = new RecentlyBlogInfoEntity();
+                        String name = e.select("a").text();
+                        String addr = e.select("a").attr("href");
+                        String auth = e.select("span").get(0).text();
+                        String source = e.select("span").get(1).text();
+                        String date = e.select("span").get(2).text();
+                        entity.setBlogname(name);
+                        entity.setBlogaddress(addr);
+                        entity.setAuthor(auth);
+                        entity.setSource(source);
+                        entity.setDate(date);
+                        mRecentlies.add(entity);
+//                        System.out.println(name + "\n");
+//                        System.out.println(addr + "\n");
+//                        System.out.println(auth + "\n");
+//                        System.out.println(source + "\n");
+//                        System.out.println(name + "\n");
+                    }
+                    result = "OK";
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    result = "ERR";
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                if (s.equals("OK")) {
+                    RecentlyBlogAdapter adapter = new RecentlyBlogAdapter(TApplication.mContext, mRecentlies);
+                    adapter.setOnItemClickListener(FrameworkPresenter.this);
+                    view.setBlogAdapter(adapter);
+                } else {
+                    view.showShorToast("数据请求失败！！！");
+                    return;
+                }
+            }
+        }.execute();
+    }
+
+
+    @Override
+    public void onItemClick(ClassifyEntity entity, RecyclerView.ViewHolder viewHolder) {
+        view.onCategoryClick(entity);
+    }
+
+    @Override
+    public void onBlogNameClickListener(int position) {
+        view.onBlogClick(mRecentlies.get(position));
+    }
+
+    @Override
+    public void onClassifyNameClickListener(int position) {
+
+    }
+}
--- app/src/main/java/com/android/sjq/wanandroid02/views/fragment/FrameworkFragment.java
+++ app/src/main/java/com/android/sjq/wanandroid02/views/fragment/FrameworkFragment.java
@@ -1,17 +1,29 @@
 package com.android.sjq.wanandroid02.views.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.sjq.wanandroid02.R;
+import com.android.sjq.wanandroid02.adapters.FirstCategoryAdapter;
+import com.android.sjq.wanandroid02.adapters.RecentlyBlogAdapter;
+import com.android.sjq.wanandroid02.base.BaseFragment;
+import com.android.sjq.wanandroid02.modles.ClassifyEntity;
+import com.android.sjq.wanandroid02.modles.RecentlyBlogInfoEntity;
+import com.android.sjq.wanandroid02.presenters.FrameworkPresenter;
+import com.android.sjq.wanandroid02.views.activities.BlogDetailActivity;
+import com.android.sjq.wanandroid02.views.view.FrameworkView;
 
 
 //项目常用框架Fragment
-public class FrameworkFragment extends Fragment {
+public class FrameworkFragment extends BaseFragment<FrameworkView, FrameworkPresenter> implements FrameworkView {
+    RecyclerView first_category;
+    RecyclerView blog_rv;
 
     public FrameworkFragment() {
         // Required empty public constructor
@@ -31,8 +43,26 @@ public class FrameworkFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_framework, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_framework, container, false);
+        initView(view);
+        //加载数据
+        mPresenter.initCategoryData();
+        return view;
+    }
+
+    @Override
+    protected FrameworkPresenter initPresenter() {
+        return new FrameworkPresenter();
+    }
+
+    //初始化界面
+    private void initView(View view) {
+        first_category = (RecyclerView) view.findViewById(R.id.first_category);
+        first_category.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        blog_rv = (RecyclerView) view.findViewById(R.id.blog_rv);
+        blog_rv.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
     }
 
 
@@ -46,4 +76,25 @@ public class FrameworkFragment extends Fragment {
         super.onDetach();
     }
 
+    @Override
+    public void setCategoryAdapter(FirstCategoryAdapter adapter) {
+        first_category.setAdapter(adapter);
+    }
+
+    @Override
+    public void setBlogAdapter(RecentlyBlogAdapter adapter) {
+        blog_rv.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCategoryClick(ClassifyEntity entity) {
+        mPresenter.getBlogData(entity);
+    }
+
+    @Override
+    public void onBlogClick(RecentlyBlogInfoEntity entity) {
+        Intent intent = new Intent(getActivity(), BlogDetailActivity.class);
+        intent.putExtra("url", entity.getBlogaddress());
+        startActivity(intent);
+    }
 }
